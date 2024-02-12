3d695e9e53b2312d0dcbc002b4c62b108a797b57
Sunphetcharaj <Sunphetcharaj123@gmail.com>
2024-02-12 00:43:09 -0800

    w

--- index.js
+++ index.js
@@ -2,80 +2,28 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
-if(direction === null){
-    character.src = 'assets/green-character/static.gif'
-}
-if(direction === 'west'){
-    character.src = 'assets/green-character/west.gif'
-}
-if(direction === 'north'){
-    character.src = 'assets/green-character/north.gif'
-}
-if(direction === 'east'){
-    character.src = 'assets/green-character/east.gif'
-}
-if(direction === 'south'){
-    character.src = 'assets/green-character/south.gif'
-}
-move(character).withArrowKeys(100, 250)
-setInterval(function(){ 
+
+function moveCharacter(direction){
+    if(direction === null){
+        character.src = `assets/green-character/static.gif`
+    }
     if(direction === 'west'){
-        x = x - 1
+        character.src = `assets/green-character/west.gif`
     }
     if(direction === 'north'){
-        y = y + 1
+        character.src = `assets/green-character/north.gif`
     }
     if(direction === 'east'){
-        x = x + 1
+        character.src = `assets/green-character/east.gif`
     }
     if(direction === 'south'){
-        y = y - 1
+        character.src = `assets/green-character/south.gif`
     }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}, 1)
-
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function(e){
-    direction = null
-})
-
-function move(element) {
-    element.style.position = 'fixed'
-
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
-    }
-    return {
-        to: moveToCoordinates,
-        withArrowKeys: moveWithArrowKeys
-    }
-    function moveWithArrowKeys(left, bottom){
-       
-    }
-
-        
 }
+
+move(character).withArrowKeys(100, 250, moveCharacter)
+
+
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
